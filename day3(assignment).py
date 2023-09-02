8e893295ec5314dcc28c54b20d2f4ed9834ae6d1
divyadharshan12 <143814596+divyadharshan12@users.noreply.github.com>
2023-09-02 18:59:59 +0530

    Create day3(assignment).py

---
+++ day3(assignment).py
@@ -0,0 +1,12 @@
+"""Write a program that takes side of a square as input and calculates the area of this square by using area() function."""
+#Code:
+
+def area_of_square(s):
+  return s*s
+a=int(input("Enter Side of a square : "))
+print("Area of Square : ",area_of_square(a))
+
+"""Sample Input and Output:
+Enter Side of a square : 14
+Area of Square :  196
+"""
